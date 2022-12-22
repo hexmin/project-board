e61e3da03e5d4718ff8ed36a09cf00fb2a3ed4fd
KHM <sbcdks124@naver.com>
2022-12-23 00:04:58 +0900

    #16 - Repository Inerface
    이 파일들로 DB에 접근.

---
+++ src/main/java/com/bitstudy/app/repository/Ex05_ArticleCommentRepository_기본테스트용.java
@@ -0,0 +1,13 @@
+package com.bitstudy.app.repository;
+
+import com.bitstudy.app.domain.ArticleComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/** TDD 를 위해서 임시로 만들어 놓은 저장소 (이거로 DB에 접근할거다)
+ *
+ *  - TDD 만드는 방법
+ *  1) 마우스 우클릭 > Go To > Test (ctrl + shift + T)
+ *  2) JUnit5 버전인지 확인
+ * */
+public interface Ex05_ArticleCommentRepository_기본테스트용 extends JpaRepository<ArticleComment, Long> {
+}
